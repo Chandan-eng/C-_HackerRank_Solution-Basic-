c2fdc477951ce4f4d34ae59b9502da049d10885c
Chandan Kumar Mishra <78314708+Chandan-eng@users.noreply.github.com>
2021-07-24 17:39:53 +0530

    Basic Data Types HackerRank
    Basic Data Types
    HackerRank Problem Solutions

---
+++ basicDataType.cpp
@@ -0,0 +1,22 @@
+# C-_HackerRank_Solution-Basic-
+C++ HackerRank Problem All Solution  
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<setprecision(20)<<a<<endl;
+    cout<<setprecision(20)<<b<<endl;
+    cout<<c<<endl;
+    cout<<setprecision(20)<<d<<endl;
+    cout<<setprecision(20)<<e<<endl;
+    return 0;
+}
+
