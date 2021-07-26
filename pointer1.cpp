9da65a1dff2062512783fe65a70a8203f7cca06e
Chandan Kumar Mishra <78314708+Chandan-eng@users.noreply.github.com>
2021-07-26 23:37:29 +0530

    pointer in cpp
    Cpp pointer Solution in HackerRank Solution

---
+++ pointer1.cpp
@@ -0,0 +1,18 @@
+# C-_HackerRank_Solution-Basic-
+//C++ HackerRank Problem All Solution  
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function    
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
